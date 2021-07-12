822555ee1312f91e721f8f5f1340fa0e53db1f1e
Brendon <61479907+brendongvieira@users.noreply.github.com>
2021-07-12 12:30:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('Jul 01, 2021 00:00:00').getTime();
+    const countDate = new Date('Dec 31, 2021 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
